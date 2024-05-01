54ca86d1d8f0a52af1b2b0af794916b32801f17d
Md Monzur Alam <67516889+monzurprofessional@users.noreply.github.com>
2024-05-01 21:17:58 +0600

    all added

--- src/hw7.java
+++ src/hw7.java
@@ -2,10 +2,8 @@
 question: Write a Java program that asks the user for a range, a starting number(inclusive)
  and an ending number (inclusive). Then, take another input for checking. If the product of
  all the digits of each number in the range is divisible by the third input, then print the product.
-** Sample Input 1: **
-25 30 4
-** Sample Output 1: **
-12 16 0
+** Sample Input 1:  25 30 4 **
+** Sample Output 1:  12 16 0 **
 **Explanation: The product of all the digits of each number from 25 to 30 are 2 × 5, 2 × 6, 2 × 7, 2 × 8, 2 × 9 and 3 × 0.
  The final products are 10, 12, 14, 16, 18 and 0 respectively. Out of these numbers only 12, 16 and 0 are divisible by
  the third input 4 and therefore they are printed.**
